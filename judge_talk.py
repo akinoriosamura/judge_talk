3c007a3d453312f731cf131574f6ae19f0445841
akinoriosamura <osamura.akinori@gmail.com>
2019-07-30 02:12:26 +0900

    update

--- judge_talk.py
+++ judge_talk.py
@@ -7,7 +7,9 @@ import codecs
 import datetime
 import locale
 
-def transcribe_gcs(gcs_uri):
+# sample: python judge_talk.py gs://judge_talk/sample.flac
+
+def transcribe_gcs(gcs_uri, save_name):
     from google.cloud import speech
     from google.cloud.speech import enums
     from google.cloud.speech import types
@@ -15,18 +17,17 @@ def transcribe_gcs(gcs_uri):
 
     audio = types.RecognitionAudio(uri=gcs_uri)
     config = types.RecognitionConfig(
-        sample_rate_hertz=16000,
+        sample_rate_hertz=48000,
         encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-        language_code='en-US')
+        #language_code='en-US')
+        language_code='ja-JP')
 
     operation = client.long_running_recognize(config, audio)
 
     print('Waiting for operation to complete...')
     operationResult = operation.result()
 
-    d = datetime.datetime.today()
-    today = d.strftime("%Y%m%d-%H%M%S")
-    fout = codecs.open('output{}.txt'.format(today), 'a', 'shift_jis')
+    fout = codecs.open(save_name, 'a', 'utf-8')
 
     for result in operationResult.results:
       for alternative in result.alternatives:
@@ -40,4 +41,15 @@ if __name__ == '__main__':
     parser.add_argument(
         'path', help='GCS path for audio file to be recognized')
     args = parser.parse_args()
-    transcribe_gcs(args.path)
+    d = datetime.datetime.today()
+    today = d.strftime("%Y%m%d-%H%M%S")
+    save_name = 'output{}.txt'.format(today)
+    transcribe_gcs(args.path, save_name)
+
+    with open(save_name) as f:
+        content = f.read()
+        print(len(content))
+        if len(content) > 100:
+            print("talking video")
+        else:
+            print("not talking video")
--- video2voice.py
+++ video2voice.py
@@ -5,12 +5,12 @@ import subprocess
 """
 mp4 to flac and judge talk or not by google api
 ref: https://www.kinacon-blog.work/entry/2018/10/23/210211
-sample: python change_video2voice.py ./videos/spider_short.mp4 ./voices/noisy.aac
+sample: python video2voice.py ./videos/sample.mp4 ./voices/sample.flac
 """
 
 assert len(sys.argv) == 3, "please set video path in argv1 and saving voice path in argv2"
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-cmd = "ffmpeg -y -i {} -vcodec copy -af volume=10dB {}".format(input_file, output_file)
+cmd = "ffmpeg -y -i {} -vcodec copy -af volume=10dB -ac 1 {}".format(input_file, output_file)
 resp = subprocess.check_output(cmd, shell=True)
